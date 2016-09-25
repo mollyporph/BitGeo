73d45c3f4c82e1efd7fb740ef49718c04ced10bf
Otto Remse <otto.remse@gmail.com>
2016-09-26 00:30:33 +0200

    fixed dottesting

--- GeoPoc/MainWindow.xaml.cs
+++ GeoPoc/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,51 +22,160 @@ namespace GeoPoc
     /// </summary>
     public partial class MainWindow : Window
     {
+        SetCollection<GeoSquareData> geoRepo = new SetCollection<GeoSquareData>();
+        SetCollection<Dot> dots = new SetCollection<Dot>();
+        GeoSquareData root = new GeoSquareData
+        {
+            Id = 1,
+            level = 0,
+            center = new Vector(380, 350),
+            Height = 700,
+            Width = 760
+        };
         public MainWindow()
         {
             InitializeComponent();
-            int padH = 50;
-            int padV = 50;
-            var geoLookup = new Dictionary<int,GeoSquareData>();
-            GeoSquare root = new GeoSquare
+            geoRepo.CollectionChanged += GeoRepo_CollectionChanged;
+        }
+
+        private void GeoRepo_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            PaintTree(geoRepo);
+            PaintDots(dots);
+        }
+        private void mapCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            mapCanvas.Children.Clear();
+            Point p = e.GetPosition(mapCanvas);
+            Dot dot = new Dot(p.X, p.Y);
+            dots.Add(dot);
+            BuildTreeFromDot(root, geoRepo, dot, 12);
+            BuildDataLabel(dot);
+        }
+        public void BuildDataLabel(Dot dot)
+        {
+            var sb = new StringBuilder();
+            sb.Append($"Data for point: {dot.Id}\n\n\n");
+            var stopWatches = new List<Stopwatch>();
+
+            for(int i = 0; i<= 12; i++)
             {
-                data = new GeoSquareData
+                var watch = Stopwatch.StartNew();
+                var geoid = dot.geoId >> i;
+                var dotCount = dots.Where(x => x.geoId >> i  == geoid).Count()-1;
+                watch.Stop();
+                stopWatches.Add(watch);
+                var square = geoRepo.FirstOrDefault(x => x.Id == geoid);
+
+                sb.Append($"Neighbours in avg {Math.Floor(square.Hypotenuse)} distance aways: {dotCount}\n");
+
+            }
+            var avg = stopWatches.Average(x => x.ElapsedTicks);
+            var ns = ((double)avg / (double)Stopwatch.Frequency) * 1000000000;
+            sb.Append($"Neighbours fetched in avg {Math.Floor(ns)} nanoseconds ({Math.Floor(avg)} ticks)");
+            Data.Text = sb.ToString();
+            
+        }
+        public void PaintDots(SetCollection<Dot> dots)
+        {
+            foreach (var dot in dots)
+            {
+                var isLast = dots.IndexOf(dot) == dots.Count - 1;
+                var dotCircle = new Ellipse
                 {
-                    Id = 1,
-                    level = 0,
-                    center = new Vector(250, 250),
-                    Height = 500,
-                    Width = 500
-                }
-            };
-            geoLookup.Add(root.data.Id,root.data);
-            BuildTree(root, geoLookup,6);
-            int i = 1;
-            foreach( var node in geoLookup)
+                    Width = isLast ? 5.5 :3.7,
+                    Height = isLast ? 5.5 : 3.7
+                };
+                dotCircle.Fill = new SolidColorBrush(isLast ? Colors.Blue : Colors.Green);
+                Canvas.SetLeft(dotCircle, dot.position.X);
+                Canvas.SetTop(dotCircle, dot.position.Y);
+                mapCanvas.Children.Add(dotCircle);
+            }
+        }
+        public void PaintTree(SetCollection<GeoSquareData> geoRepo)
+        {
+            foreach (var node in geoRepo)
             {
                 var rec = new Rectangle
                 {
-                    Height = node.Value.Height,
-                    Width = node.Value.Width
+                    Height = node.Height,
+                    Width = node.Width
 
                 };
-
-                var varyingColorBrush = new SolidColorBrush(Color.FromArgb(255, 255,0,0));
+                var varyingColorBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
                 rec.StrokeThickness = 0.7;
                 rec.Stroke = varyingColorBrush;
                 rec.Fill = new SolidColorBrush(Colors.Transparent);
-
-                Canvas.SetTop(rec, node.Value.Y+padV);
-                Canvas.SetLeft(rec, node.Value.X+padH);
+                Canvas.SetTop(rec, node.Y);
+                Canvas.SetLeft(rec, node.X);
                 mapCanvas.Children.Add(rec);
-                var numtext = new TextBlock();
-                numtext.Text = node.Value.Id.ToString();
-                numtext.Foreground = new SolidColorBrush(Colors.Green);
-                Canvas.SetTop(numtext, node.Value.center.Y+padV -7);
-                Canvas.SetLeft(numtext, node.Value.center.X + padH -6);
-                mapCanvas.Children.Add(numtext);
-                i++;
             }
+        }
+        public void BuildTreeFromDot(GeoSquareData currentNode, SetCollection<GeoSquareData> geoRepo, Dot dot, int maxLevel = 5)
+        {
+            geoRepo.Add(currentNode);
+            if (currentNode.level == maxLevel)
+                return;
+
+            if(currentNode.level % 2 == 0)
+            {
+                if(dot.position.Y >= currentNode.center.Y)
+                {
+                    var Higherleaf = new GeoSquareData
+                    {
+                        level = currentNode.level + 1,
+                        Id = currentNode.Id << 1 | 1,
+                        center = new Vector(currentNode.center.X, currentNode.center.Y + (currentNode.Height / 4)),
+                        Height = currentNode.Height / 2,
+                        Width = currentNode.Width
+                    };
+                    dot.geoId = Higherleaf.Id;
+                    BuildTreeFromDot(Higherleaf, geoRepo, dot, maxLevel);
+                }
+                else
+                {
+                    var Lowerleaf = new GeoSquareData
+                    {
+                        level = currentNode.level + 1,
+                        Id = currentNode.Id << 1 | 0,
+                        center = new Vector(currentNode.center.X, currentNode.center.Y - (currentNode.Height / 4)),
+                        Height = currentNode.Height / 2,
+                        Width = currentNode.Width
+                    };
+                    dot.geoId = Lowerleaf.Id;
+                    BuildTreeFromDot(Lowerleaf, geoRepo, dot, maxLevel);
+                }
+            }
+            else
+            {
+                if (dot.position.X >= currentNode.center.X)
+                {
+                    var Higherleaf = new GeoSquareData
+                    {
+                        level = currentNode.level + 1,
+                        Id = currentNode.Id << 1 | 1,
+                        center = new Vector(currentNode.center.X + (currentNode.Width / 4), currentNode.center.Y),
+                        Height = currentNode.Height,
+                        Width = currentNode.Width / 2
+                    };
+                    dot.geoId = Higherleaf.Id;
+                    BuildTreeFromDot(Higherleaf, geoRepo, dot, maxLevel);
+                }
+                else
+                {
+                    var Lowerleaf = new GeoSquareData
+                    {
+                        level = currentNode.level + 1,
+                        Id = currentNode.Id << 1 | 0,
+                        center = new Vector(currentNode.center.X - (currentNode.Width / 4), currentNode.center.Y),
+                        Height = currentNode.Height,
+                        Width = currentNode.Width / 2
+                    };
+                    dot.geoId = Lowerleaf.Id;
+                    BuildTreeFromDot(Lowerleaf, geoRepo, dot, maxLevel);
+                }
+            }
+
         }
         public void BuildTree(GeoSquare parent, Dictionary<int, GeoSquareData> geolookup, int maxLevel = 5)
         {
@@ -76,7 +187,7 @@ namespace GeoPoc
                 data = new GeoSquareData
                 {
                     level = level,
-                    Id = parent.data.Id << 1 | 0,
+                    Id = parent.data.Id << 1 | 0
 
                 }
             };
@@ -114,8 +225,10 @@ namespace GeoPoc
             BuildTree(parent.lower, geolookup,maxLevel);
             BuildTree(parent.higher, geolookup, maxLevel);
         }
+
+
     }
-    
+
     public class GeoSquareData
     {
         public double X => center.X - Width / 2.0;
@@ -126,7 +239,7 @@ namespace GeoPoc
         public int level { get; set; }
         public Vector center { get; set; }
         public int Id { get; set; }
-        public double Hypotenuse => Math.Sqrt(Math.Pow(center.X, 2) + Math.Pow(center.Y, 2));
+        public double Hypotenuse => Math.Sqrt(Math.Pow(Height, 2) + Math.Pow(Width, 2));
     }
     public class GeoSquare
     {
@@ -134,4 +247,31 @@ namespace GeoPoc
         public GeoSquare lower { get; set; }
         public GeoSquare higher { get; set; }
     }
+    public class Dot
+    {
+        public Dot(double X, double Y) { this.position = new Vector(X, Y); }
+        public Guid Id => Guid.NewGuid();
+        public Vector position { get; set; }
+        public int geoId { get; set; }
+
+    }
+    public class SetCollection<T> : ObservableCollection<T>
+    {
+        protected override void InsertItem(int index, T item)
+        {
+            if (Contains(item)) return;
+
+            base.InsertItem(index, item);
+        }
+
+        protected override void SetItem(int index, T item)
+        {
+            int i = IndexOf(item);
+            if (i >= 0 && i != index) throw new ArgumentException(nameof(item));
+
+            base.SetItem(index, item);
+        }
+    }
 }
+
+
